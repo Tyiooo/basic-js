dfd970243119de1226bcae2b45b6950aeb3b4a29
Tyiooo <vailygione@gmail.com>
2023-04-17 23:24:44 +0300

    feat: done some tasks

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,11 +22,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function getDNSStats(domains) {
+  let result ={}
+  for(let i = 0; i < domains.length; i++){
+    const domParts = domains[i].split('.').reverse();
+    let curPart = '';
+
+    for (let j = 0; j < domParts.length; j++) {
+      curPart += `.${domParts[j]}`;
+      if(result[curPart]){
+        result[curPart] += 1;
+      }
+      else result[curPart] = 1;
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
   getDNSStats
 };
+
+
+domains = [
+    'code.yandex.ru',
+    'music.yandex.ru',
+    'yandex.ru'
+   ]
+console.log(getDNSStats(domains));
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,50 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+	str = String(str);
+
+	let repeatTimes;
+	if(options.repeatTimes) repeatTimes = options.repeatTimes;
+	else repeatTimes = 1;
+
+	let additionRepeatTimes;
+	if(options.additionRepeatTimes) additionRepeatTimes = options.additionRepeatTimes;
+	else additionRepeatTimes = 1;
+
+	let res = "";
+	let separator;
+	if(options.separator) separator = options.separator;
+	else separator = "+";
+
+	console.log(`${options.addition}`);
+	let addition = [];
+	let additionSeparator;
+	if(options.additionSeparator) additionSeparator = options.additionSeparator;
+	else additionSeparator = "|";
+
+	if(String(options.addition) !== 'undefined'){
+		for(let i = 0; i < additionRepeatTimes; i++){
+			addition.push(`${options.addition}`);
+		}
+	}
+
+	
+	addition = addition.join(`${additionSeparator}`);
+
+	str += addition;
+	let tempArr = [];
+
+	for(let i = 0; i < repeatTimes; i++)
+	{
+		tempArr.push(str);
+	}
+
+	res = tempArr.join(`${separator}`);
+
+	return res;
+
+
 }
 
 module.exports = {
